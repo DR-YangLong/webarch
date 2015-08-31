6b0588dfc0047fa218a7dee7a0a8beea0c7e0643
杨龙 <410357434@163.com>
2015-08-31 11:11:08 +0800

    JSR-303验证错误信息显示

--- web/src/main/java/com/webarch/front/web/passport/RegisterController.java
+++ web/src/main/java/com/webarch/front/web/passport/RegisterController.java
@@ -8,7 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.Errors;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,7 +18,7 @@ import javax.validation.Valid;
  * package: com.webarch.front.web.passport <br/>
  * blog:<a href="http://dr-yanglong.github.io/">dr-yanglong.github.io</a><br/>
  * <p>
- * functional describe:注册控制器
+ * functional describe:注册控制器，演示使用JSR-303验证框架，错误展示视图为regIndex.ftl,分单个错误提示
  *
  * @author DR.YangLong [410357434@163.com]
  * @version 1.0    2015/8/18 19:38
@@ -33,22 +33,27 @@ public class RegisterController {
      * 使用自动验证数据
      *
      * @param user   用户对象
-     * @param errors 验证错误对象
+     * @param bindingResult 验证信息绑定对象
      * @return
      */
     @RequestMapping("/reg")
-    public ModelAndView register(@Valid User user, Errors errors) {
+    public ModelAndView register(@Valid User user, BindingResult bindingResult) {
         ModelAndView modelAndView = ViewGenerator.getModelAndView();
-        if (errors.hasErrors()) {//验证出错
+        if (bindingResult.hasErrors()) {//验证出错
             modelAndView.setViewName("/front/passport/regIndex");
         } else {
-            try {
-                userService.insertUser(user);
-                modelAndView.addObject("User", user);
-                //TODO 自动登录
-            } catch (Exception e) {
-                log.error("insert user to db error!obj:" + JSON.toJSONString(user), e);
-                modelAndView.setViewName("/front/passport/regIndex");
+            if("410357434@163.com".equals(user.getUsername())){//用户已注册
+                //手动注入验证错误信息
+               bindingResult.rejectValue("username","用户名已注册","用户名已注册");
+            }else {
+                try {
+                    userService.insertUser(user);
+                    modelAndView.addObject("User", user);
+                    //TODO 自动登录
+                } catch (Exception e) {
+                    log.error("insert user to db error!obj:" + JSON.toJSONString(user), e);
+                    modelAndView.setViewName("/front/passport/regIndex");
+                }
             }
         }
         return modelAndView;
--- web/src/main/webapp/template/default/front/passport/regIndex.ftl
+++ web/src/main/webapp/template/default/front/passport/regIndex.ftl
@@ -6,9 +6,24 @@
 </head>
 <body>
 <form action="/reg" method="post">
-    用户名：<input type="text" value="" name="username" maxlength="36"><br/>
-    密码：<input type="password" value="" name="username" maxlength="36"><br/>
-    角色：<input type="text" value="" name="roles"><br/>
+    用户名：<input type="text" value="${(user.username)!}" name="username" maxlength="36"><br/>
+    <#--如果返回了用户对象，说明验证没有通过-->
+<#if user??>
+    <#--验证的属性值-->
+    <@spring.bind "user.username" />
+    <#--错误信息-->
+    <@spring.showErrors "<br/>"/>
+</#if>
+    密码：<input type="password" value="${(user.password)!}" name="password" maxlength="36"><br/>
+<#if user??>
+    <@spring.bind "user.password" />
+    <@spring.showErrors "<br/>"/>
+</#if>
+    角色：<input type="text" value="${(user.roles)!}" name="roles"><br/>
+<#if user??>
+    <@spring.bind "user.roles" />
+    <@spring.showErrors "<br/>"/>
+</#if>
     <input type="submit" value="注册">
 </form>
 </body>
